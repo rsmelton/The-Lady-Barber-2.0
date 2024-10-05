48229cf0ca41a3c91b14e36241c92d8b82d9e639
Robert Melton <rsmelton@crimson.ua.edu>
2024-10-05 18:41:14 -0500

    Testing scrolling for mobile

--- The-Lady-Barber-2.0/page_loader.js
+++ The-Lady-Barber-2.0/page_loader.js
@@ -14,13 +14,13 @@ async function load_home_page(section) {
     // then depending on the section we want, we then scroll to that section.
     if (section === 'about') {
         var about_content = document.getElementById('about');
-        about_content.scrollIntoView({ behavior: 'smooth' });
+        about_content.scrollIntoView({ behavior: 'auto' });
     } else if (section === 'services') {
         var services_content = document.getElementById('services');
-        services_content.scrollIntoView({ behavior: 'smooth' });
+        services_content.scrollIntoView({ behavior: 'auto' });
     } else if (section === 'hours-location') {
         var hours_location_content = document.getElementById('hours-location');
-        hours_location_content.scrollIntoView({ behavior: 'smooth' });
+        hours_location_content.scrollIntoView({ behavior: 'auto' });
     }
 }
 
