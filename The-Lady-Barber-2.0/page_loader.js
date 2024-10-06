dba32165bab5426b40f78b5c0dfa2b6af10cbce9
Robert Melton <rsmelton@crimson.ua.edu>
2024-10-05 20:49:42 -0500

    Still trying to fix scrolling

--- The-Lady-Barber-2.0/page_loader.js
+++ The-Lady-Barber-2.0/page_loader.js
@@ -10,25 +10,21 @@
 // This loads the home page when first loading the page
 // since we are loading pages dynamically
 // document.addEventListener('DOMContentLoaded', function() {
-//     // var page_content = document.getElementById('page_content');
 //     fetch('home.html')
 //         .then(response => response.text())
-//         .then(html => {
-//             document.getElementById('page_content').innerHTML = html;
-//         });
+//         .then(html => document.getElementById('page_content').innerHTML = html);
 // });
+
+document.addEventListener('DOMContentLoaded', function() {
+    load_home_page();
+});
 
 // Home page loader with scroll functionality for different sections.
 function load_home_page(section) {
     // first we load the home page
-    // var page_content = document.getElementById('page_content');
-    // page_content.innerHTML = await (await fetch('home.html')).text();
-
     fetch('home.html')
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('page_content').innerHTML = html;
-    });
+        .then(response => response.text())
+        .then(html => document.getElementById('page_content').innerHTML = html);
 
     // then depending on the section we want, we then scroll to that section.
     // if (section === 'about') {
@@ -42,10 +38,15 @@ function load_home_page(section) {
     //     hours_location_content.scrollIntoView({ behavior: 'smooth' });
     // }
 
-    if (section === 'about')               var content = document.getElementById('about');
-    else if (section === 'services')       var content = document.getElementById('services');
-    else if (section === 'hours-location') var content = document.getElementById('hours-location');
-    content.scrollIntoView({ behavior: 'smooth' });
+    // if      (section === '')               return;
+    // else if (section === 'about')          var content = document.getElementById('about');
+    // else if (section === 'services')       var content = document.getElementById('services');
+    // else if (section === 'hours-location') var content = document.getElementById('hours-location');
+    // content.scrollIntoView({ behavior: 'smooth' });
+
+    if      (section === 'about')          document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    else if (section === 'services')       document.getElementById('services').scrollIntoView({ behavior: 'smooth' });
+    else if (section === 'hours-location') document.getElementById('hours-location').scrollIntoView({ behavior: 'smooth' });
 }
 
 // Gallery page loader
