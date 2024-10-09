5d8ea079d40089eff513d72f18eabcb6d454ec41
Robert Melton <rsmelton@crimson.ua.edu>
2024-10-09 17:54:24 -0500

    Finally fixed navbar/header to not be scrollable and now scrolling functionality works

--- The-Lady-Barber-2.0/page_loader.js
+++ The-Lady-Barber-2.0/page_loader.js
@@ -1,20 +1,39 @@
-var page_content = document.getElementById('page_content');
-var home_page;
-var gallery_page;
+function scrollToSection(event, sectionId) {
 
-document.addEventListener('DOMContentLoaded', async function() {
-    // We fetch the home page and after we get the html back we can then load the page
-    // by default since we are loading content dynamically and the home page would be our default page
-    home_page = await ((await fetch('home.html')).text());
-    gallery_page = await (await fetch('gallery.html')).text();
-    load_home_page();
-});
+    event.preventDefault();
 
-function load_home_page(section) {
-    page_content.innerHTML = home_page;
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+
+    // let sectionYOffset = document.getElementById(section).offsetTop;
+    // window.scrollTo(0, sectionYOffset);
+    // document.getElementById(section).scrollTop(sectionYOffset);
 }
 
-async function load_gallery_page() {
-    page_content.innerHTML = gallery_page;
-    window.scrollTo(0,0);
-}
+
+// Old Code for fetching pages dynamically
+
+// var page_content = document.getElementById('page_content');
+// var home_page;
+// var gallery_page;
+
+// document.addEventListener('DOMContentLoaded', async function() {
+//     // We fetch the home page and after we get the html back we can then load the page
+//     // by default since we are loading content dynamically and the home page would be our default page
+//     home_page = await (await fetch('home.html')).text();
+//     gallery_page = await (await fetch('gallery.html')).text();
+//     load_home_page();
+// });
+
+// function load_home_page(section) {
+//     page_content.innerHTML = home_page;
+//     // document.getElementById(section).scrollIntoView({ behavior: 'smooth' });
+    // let sectionYOffset = document.getElementById(section).offsetTop;
+    // document.getElementById(section).scrollTop(sectionYOffset);
+//     // window.scrollTo(0, sectionYOffset);
+// }
+
+// function load_gallery_page() {
+//     page_content.innerHTML = gallery_page;
+//     window.scrollTo(0,0);
+// }
