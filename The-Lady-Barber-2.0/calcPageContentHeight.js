709719e6dc4fe54fdf36f4ed0ac706631e7885a7
Robert Melton <rsmelton@crimson.ua.edu>
2024-10-11 17:43:05 -0500

    Trying to fix the bug where a white strip appears at bottom of screen when resizing the window sometimes

--- The-Lady-Barber-2.0/calcPageContentHeight.js
+++ The-Lady-Barber-2.0/calcPageContentHeight.js
@@ -1,6 +1,13 @@
 // Get the height of the navbar along with the page content
-let navbarHeight = document.getElementById('navbar').offsetHeight;
-let pageContent = document.getElementById('page_content');
+var navbarHeight = document.getElementById('navbar').offsetHeight;
+var pageContent = document.getElementById('page_content');
 
-// set the page contents height based off the windows height - navbar height;
-pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
+// Make sure to calculate the height of the content when loading the page
+// and when resizing the page
+window.addEventListener('load', () => {
+    pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
+});
+
+window.addEventListener('resize', () => {
+    pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
+});
