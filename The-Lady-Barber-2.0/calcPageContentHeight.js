6893ac77b226725b2d5cff9334b40429aaf3cd9a
Robert Melton <rsmelton@crimson.ua.edu>
2024-10-11 17:45:55 -0500

    Might not need to specify an event listener for loading, instead we can just calculate by default and with resize events

--- The-Lady-Barber-2.0/calcPageContentHeight.js
+++ The-Lady-Barber-2.0/calcPageContentHeight.js
@@ -1,12 +1,13 @@
 // Get the height of the navbar along with the page content
 var navbarHeight = document.getElementById('navbar').offsetHeight;
 var pageContent = document.getElementById('page_content');
+pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
 
 // Make sure to calculate the height of the content when loading the page
 // and when resizing the page
-window.addEventListener('load', () => {
-    pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
-});
+// window.addEventListener('load', () => {
+//     pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
+// });
 
 window.addEventListener('resize', () => {
     pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
