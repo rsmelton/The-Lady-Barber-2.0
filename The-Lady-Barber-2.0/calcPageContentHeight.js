6c3f8d9a45a4ec83039bb033417895e8e15f998e
Robert Melton <rsmelton@crimson.ua.edu>
2024-10-26 00:39:37 -0500

    Experimenting

--- The-Lady-Barber-2.0/calcPageContentHeight.js
+++ The-Lady-Barber-2.0/calcPageContentHeight.js
@@ -1,6 +1,7 @@
 // Get the height of the navbar along with the page content
 var navbarHeight = document.getElementById('navbar').offsetHeight;
 var pageContent = document.getElementById('page_content');
+pageContent.style.height = (document.documentElement.scrollHeight - navbarHeight) + 'px';
 // pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
 
 // Make sure to calculate the height of the content when loading the page
@@ -10,9 +11,17 @@ var pageContent = document.getElementById('page_content');
 // });
 
 window.onload(() => {
-    pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
+    pageContent.style.height = (document.documentElement.scrollHeight - navbarHeight) + 'px';
 });
 
 window.addEventListener('resize', () => {
-    pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
+    pageContent.style.height = (document.documentElement.scrollHeight - navbarHeight) + 'px';
 });
+
+// window.onload(() => {
+//     pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
+// });
+
+// window.addEventListener('resize', () => {
+//     pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
+// });
