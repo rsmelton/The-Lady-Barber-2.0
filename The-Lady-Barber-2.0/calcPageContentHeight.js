1c1d332b7bfb5e0518868223b8276898d0e76538
Robert Melton <rsmelton@crimson.ua.edu>
2024-10-26 00:51:37 -0500

    Back to the table

--- The-Lady-Barber-2.0/calcPageContentHeight.js
+++ The-Lady-Barber-2.0/calcPageContentHeight.js
@@ -1,21 +1,12 @@
 // Get the height of the navbar along with the page content
 var navbarHeight = document.getElementById('navbar').offsetHeight;
 var pageContent = document.getElementById('page_content');
-// pageContent.style.height = (document.documentElement.scrollHeight - navbarHeight) + 'px';
 pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
 
 // Make sure to calculate the height of the content when loading the page
 // and when resizing the page
 // window.addEventListener('DOMContentLoaded', () => {
 //     pageContent.style.height = (window.innerHeight - navbarHeight) + 'px';
-// });
-
-// window.onload = () => {
-//     pageContent.style.height = (document.documentElement.scrollHeight - navbarHeight) + 'px';
-// };
-
-// window.addEventListener('resize', () => {
-//     pageContent.style.height = (document.documentElement.scrollHeight - navbarHeight) + 'px';
 // });
 
 window.onload = () => {
